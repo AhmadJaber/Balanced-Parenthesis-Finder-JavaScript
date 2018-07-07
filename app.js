4df10769b68533a8412f739c4da3a9b1dd09847a
AhmadJaber <programmarashik@gmail.com>
2018-07-07 14:42:28 +0600

    Code Modified

--- app.js
+++ app.js
@@ -1,44 +1,59 @@
+const btnUI = document.querySelector('#btn');
+const inputParenUI = document.querySelector('#input-paren');
 
-const btn = document.querySelector('#btn');
-const inputParen = document.querySelector('#input-paren');
-
-btn.addEventListener('click', isBalanced);
-inputParen.addEventListener('keyup', function(e) {
-  e.preventDefault(); // if it is form then i used that
-
-  if (e.keyCode === 13) {
-    isBalanced();
-  }
-})
+btnUI.addEventListener('click', isBalanced);
+inputParenUI.addEventListener('keyup', function(e) {
+    if(e.keycode === 13) {
+        isBalanced();
+    }
+});
 
 function isBalanced() {
-  const openParen = {'{':'}', '[': ']', '(': ')'};
-  const closedParen = {'}': true, ']': true, ')': true};
-  let stack = [];
+    let stack = [];
+    const openParen = {'{': '}', '[': ']', '(': ')'};
+    const closeParen = {'}': true, ']': true, ')': true};
 
-  let inputVal = inputParen.value;
-
-  for (let i=0; i<inputVal.length; i++) {
-    let val = inputVal[i];
+    const inputVal = inputParenUI.value;
     
-    if (openParen[val]) {
-      stack.push(val);
-    }else if (closedParen[val]) {
-      if (openParen[stack.pop()] !== val) return printResult(false);
+    for(let i=0; i<inputVal.length; i++) {
+        let val = inputVal[i];
+        
+        if(openParen[val]) {
+            stack.push(val);
+        }
+        else if(closeParen[val]) {
+            if(openParen[stack.pop()] !== val) {
+                return printResult(false);
+            }
+        }
     }
-  }
 
-  let result = stack.length === 0 ? true : false;
-  printResult(result);
+    let result = stack.length === 0 ? true : false;
+    printResult(result);
 }
 
-function printResult(bool) {
-  const UIanswer = document.querySelector("#answer");
-  const inputVal = inputParen.value;
+function printResult(bol) {
+    const answerUI = document.querySelector('#answer');
+    const inputVal = inputParenUI.value;
 
-  if (bool) {
-    UIanswer.innerHTML = `WooHoo! Your string <div class='bold'>\"${inputVal}\ "</div> is <span class='bold'>balanced</span>!`;
-  } else {
-    UIanswer.innerHTML = `Ack! Your string <div class='bold dirty-font'>\"${inputVal} \"</div> is <span class='bold'>unbalanced</span>, check your string again.`;
-  }
+    if(bol) {
+        answerUI.innerHTML = `WooHoo! Your String <div class='bold'>\"${inputVal}\ "</div> is <span class='bold'>balanced</span>`;
+    }else {
+        answerUI.innerHTML = `Ack! Your string <div class='bold dirty-font'>\"${inputVal} \"</div> is <span class='bold'>unbalanced</span>, check your string again.`;
+    }
+
+    inputParenUI.value = '';
 }
+
+/**
+ * Test 
+    * balancedParens('(');  // false
+    balancedParens('()'); // true
+    balancedParens(')(');  // false
+    balancedParens('(())');  // true
+    balancedParens('[](){}'); // true
+    balancedParens('[({})]');   // true
+    balancedParens('[(]{)}'); // false
+    balancedParens('var supYo  = { hey: dog() }'); // true
+    balancedParens('var fiddle = function() { doggy.eat();'); // false
+ */
